1203a1099a09b81fd740191a1402bf76aa077ef6
Dot99 <gui.rocha.felix@gmail.com>
2025-07-04 09:57:44 +0100

    FIx: Cancel Google OAuth would crash the app

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -2,10 +2,10 @@ import speakeasy from "speakeasy"; // For two-factor authentication
 import qrcode from "qrcode"; // For generating QR codes
 import { getUserById } from "../services/usersServices.js";
 import {
-	handleGoogleCallback,
-	twoFaSetupService,
-	twoFaVerifyService,
-	twoFaLoginService,
+    handleGoogleCallback,
+    twoFaSetupService,
+    twoFaVerifyService,
+    twoFaLoginService,
 } from "../services/authService.js";
 
 /**
@@ -18,89 +18,99 @@ import {
  * It handles the callback, retrieves the user information, and sends a token back to the client.
  */
 const googleOAuthCallback = async (request, reply, fastify) => {
-	try {
-		const result = await handleGoogleCallback(request, fastify);
-		const params = new URLSearchParams({
-			token: result.token,
-			userId: result.userId.toString(),
-			twofa: result.twofa.toString(),
-			google_id: result.exists.toString(),
-		}).toString();
+    try {
+        if (request.query && request.query.error) {
+            const error = request.query.error;
+            const frontendUrl = process.env.FRONTEND_URL;
+            return reply.redirect(
+                `${frontendUrl}/?oauth_error=${encodeURIComponent(error)}`
+            );
+        }
+        const result = await handleGoogleCallback(request, fastify);
+        const params = new URLSearchParams({
+            token: result.token,
+            userId: result.userId.toString(),
+            twofa: result.twofa.toString(),
+            google_id: result.exists.toString(),
+        }).toString();
 
-		// Redirect to frontend with the token
-		const frontendUrl = process.env.FRONTEND_URL || "http://localhost:3000";
-		reply.redirect(`${frontendUrl}/?${params}`);
-	} catch (error) {
-		reply.log.error(error);
-		reply.status(500).send({ error: "Internal Server Error" });
-	}
+        // Redirect to frontend with the token
+        const frontendUrl = process.env.FRONTEND_URL;
+        reply.redirect(`${frontendUrl}/?${params}`);
+    } catch (error) {
+        reply.log.error(error);
+        reply.status(500).send({ error: "Internal Server Error" });
+    }
 };
 
 const twoFaSetup = async (request, reply, fastify) => {
-	try {
-		const userId = request.user.id;
-		const secret = speakeasy.generateSecret({
-			name: `Trans (${request.user.username})`,
-		});
-		const result = await twoFaSetupService(userId, secret.base32);
-		if (result.success) {
-			const qrCodeData = await qrcode.toDataURL(secret.otpauth_url); //Requires await!
-			reply.status(200).send({
-				success: true,
-				code: qrCodeData,
-			});
-		} else {
-			reply
-				.status(400)
-				.send({ success: false, error: messages.TwoFAFailed[request.lang] });
-		}
-	} catch (error) {
-		reply.log.error(error);
-		reply.status(500).send({ error: "2FA setup failed" });
-	}
+    try {
+        const userId = request.user.id;
+        const secret = speakeasy.generateSecret({
+            name: `Trans (${request.user.username})`,
+        });
+        const result = await twoFaSetupService(userId, secret.base32);
+        if (result.success) {
+            const qrCodeData = await qrcode.toDataURL(secret.otpauth_url); //Requires await!
+            reply.status(200).send({
+                success: true,
+                code: qrCodeData,
+            });
+        } else {
+            reply.status(400).send({
+                success: false,
+                error: messages.TwoFAFailed[request.lang],
+            });
+        }
+    } catch (error) {
+        reply.log.error(error);
+        reply.status(500).send({ error: "2FA setup failed" });
+    }
 };
 
 const twoFaVerify = async (request, reply, fastify) => {
-	try {
-		const { token } = request.body;
-		const userId = request.user.id;
-		const result = await twoFaVerifyService(userId, token, true);
-		if (result.success) {
-			reply.status(200).send({ success: true });
-		} else {
-			reply
-				.status(400)
-				.send({ success: false, error: messages.TwoFAFailed[request.lang] });
-		}
-	} catch (error) {
-		reply.log.error(error);
-		reply.status(500).send({ error: messages.TwoFAFailed[request.lang] });
-	}
+    try {
+        const { token } = request.body;
+        const userId = request.user.id;
+        const result = await twoFaVerifyService(userId, token, true);
+        if (result.success) {
+            reply.status(200).send({ success: true });
+        } else {
+            reply.status(400).send({
+                success: false,
+                error: messages.TwoFAFailed[request.lang],
+            });
+        }
+    } catch (error) {
+        reply.log.error(error);
+        reply.status(500).send({ error: messages.TwoFAFailed[request.lang] });
+    }
 };
 
 const twoFaLogin = async (request, reply, fastify) => {
-	try {
-		const { token } = request.body;
-		const userId = request.user.id;
-		const result = await twoFaLoginService(userId, token);
-		if (result.success) {
-			const user = await getUserById(userId);
-			const newToken = fastify.jwt.sign({
-				id: user.user.id,
-				username: user.user.username,
-				twofa_enabled: !!user.user.twofa_enabled,
-				twofa_verified: true,
-			});
-			return reply.status(200).send({ success: true, token: newToken });
-		} else {
-			return reply
-				.status(400)
-				.send({ success: false, error: messages.TwoFAFailed[request.lang] });
-		}
-	} catch (error) {
-		reply.log.error(error);
-		reply.status(500).send({ error: messages.TwoFAFailed[request.lang] });
-	}
+    try {
+        const { token } = request.body;
+        const userId = request.user.id;
+        const result = await twoFaLoginService(userId, token);
+        if (result.success) {
+            const user = await getUserById(userId);
+            const newToken = fastify.jwt.sign({
+                id: user.user.id,
+                username: user.user.username,
+                twofa_enabled: !!user.user.twofa_enabled,
+                twofa_verified: true,
+            });
+            return reply.status(200).send({ success: true, token: newToken });
+        } else {
+            return reply.status(400).send({
+                success: false,
+                error: messages.TwoFAFailed[request.lang],
+            });
+        }
+    } catch (error) {
+        reply.log.error(error);
+        reply.status(500).send({ error: messages.TwoFAFailed[request.lang] });
+    }
 };
 
 export { googleOAuthCallback, twoFaSetup, twoFaVerify, twoFaLogin };
